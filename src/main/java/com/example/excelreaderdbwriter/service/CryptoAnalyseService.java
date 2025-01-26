b4032d65cece2f38e20225f74c11d8664dd26d6c
micha-mn <57522990+micha-mn@users.noreply.github.com>
2025-01-26 22:49:24 +0200

    latest

--- src/main/java/com/example/excelreaderdbwriter/service/CryptoAnalyseService.java
+++ src/main/java/com/example/excelreaderdbwriter/service/CryptoAnalyseService.java
@@ -29,14 +29,14 @@ public class CryptoAnalyseService {
 	 
 	 @Transactional
 	    public boolean insertIntoTable(DataDTO dataDTO) {
-	        String sequenceQuery = "select next_val from " + dataDTO.getTableName() + "_sequence";
+	        String sequenceQuery = "select next_val from cr_" + dataDTO.getTableName() + "_sequence";
             
 	        Query sequenceNativeQuery = entityManager.createNativeQuery(sequenceQuery);
 	        BigInteger nextId = (BigInteger) sequenceNativeQuery.getSingleResult();
 
 	        Long id = nextId.longValue();
 
-	        String insertQuery = "insert into " + dataDTO.getTableName() + " (id, refer_date, value) values (:id, :referDate, :value)";
+	        String insertQuery = "insert into cr_" + dataDTO.getTableName() + " (id, refer_date, value) values (:id, :referDate, :value)";
 
 	        Query nativeInsertQuery = entityManager.createNativeQuery(insertQuery);
 
@@ -59,7 +59,7 @@ public class CryptoAnalyseService {
 	    
 	  @Transactional
 	    public void updateNextVal(String tableName) {
-	        String nativeQuery = "UPDATE " + tableName + "_sequence SET next_val = next_val + 1";
+	        String nativeQuery = "UPDATE cr_" + tableName + "_sequence SET next_val = next_val + 1";
 	        entityManager.createNativeQuery(nativeQuery).executeUpdate();
 	    }
 	 
@@ -79,7 +79,7 @@ public class CryptoAnalyseService {
 
 	public List<DataDTO> getData(String tableName) {
 	
-		 String dataQuery = "select * from " + tableName + " order by refer_date desc";
+		 String dataQuery = "select * from cr_" + tableName + " order by refer_date desc";
 		    Query dataNativeQuery = entityManager.createNativeQuery(dataQuery);
 
 		    List<Object[]> resultList = dataNativeQuery.getResultList();
@@ -102,7 +102,7 @@ public class CryptoAnalyseService {
 	@Transactional
 	public boolean updateData(DataDTO dataDTO) {
 		
-        String updateQuery = "update " + dataDTO.getTableName() + " set  `value` =  :value WHERE `id` = :id";
+        String updateQuery = "update cr_" + dataDTO.getTableName() + " set  `value` =  :value WHERE `id` = :id";
 
         Query nativeUpdateQuery = entityManager.createNativeQuery(updateQuery);
 
@@ -114,7 +114,7 @@ public class CryptoAnalyseService {
 	}
 	@Transactional
 	public boolean deleteData(String tablename, String id) {
-		  String nativeQuery = "DELETE FROM " + tablename + " where id= "+id;
+		  String nativeQuery = "DELETE FROM cr_" + tablename + " where id= "+id;
 	       entityManager.createNativeQuery(nativeQuery).executeUpdate();
 	       return true;
 	}
--- src/main/resources/mysql/schema.ddl
+++ src/main/resources/mysql/schema.ddl
@@ -1,5 +1,5 @@
-insert into ravencoin_sequence values ( 1 );
-insert into harmony_sequence values ( 1 );
-insert into bitcoin_sequence values ( 1 );
-insert into reef_sequence values ( 1 );
-insert into near_sequence values ( 1 );
+insert into cr_ravencoin_sequence values ( 1 );
+insert into cr_harmony_sequence values ( 1 );
+insert into cr_bitcoin_sequence values ( 1 );
+insert into cr_reef_sequence values ( 1 );
+insert into cr_near_sequence values ( 1 );
